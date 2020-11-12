8080363730e9ac32a2f79f9712df7a5f80ffc846
admonxetrii <nishamwagle@gmail.com>
2020-11-12 15:08:58 +0545

    Aba Documentation ko kaam baaki

--- Restaurant/models.py
+++ Restaurant/models.py
@@ -17,16 +17,17 @@ def val2(value) :
     else :
         return True
 
-class Profilepic(models.Model):
+
+class Profilepic(models.Model) :
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     image = models.ImageField(upload_to='User/', null=False, blank=False)
 
-    def __str__(self):
+    def __str__(self) :
         return self.user.username
 
-    def delete(self, *args, **kwargs):
+    def delete(self, *args, **kwargs) :
         self.image.delete()
-        super().delete(*args,**kwargs)
+        super().delete(*args, **kwargs)
 
     def save(self, *args, **kwargs) :
         # delete old file when replacing by updating the file
@@ -38,6 +39,7 @@ class Profilepic(models.Model):
             pass  # when new photo then we do nothing, normal case
         super(Profilepic, self).save(*args, **kwargs)
 
+
 class MenuCategory(models.Model) :
     title = models.CharField(max_length=100, unique=True, validators=[mycustomvalidator, val2])
     image = models.ImageField(upload_to='CategoryMenu/', null=True, blank=True)
@@ -45,9 +47,9 @@ class MenuCategory(models.Model) :
     def __str__(self) :
         return self.title
 
-    def delete(self, *args, **kwargs):
+    def delete(self, *args, **kwargs) :
         self.image.delete()
-        super().delete(*args,**kwargs)
+        super().delete(*args, **kwargs)
 
     def save(self, *args, **kwargs) :
         # delete old file when replacing by updating the file
@@ -72,27 +74,26 @@ class Menu(models.Model) :
         db_table = "menu"
 
 
-class Table(models.Model):
+class Table(models.Model) :
     title = models.CharField(max_length=100, unique=True, validators=[mycustomvalidator, val2])
-    occupied = models.IntegerField(null=True,blank=True)
-    occHrs = models.IntegerField(null=True,blank=True)
-    occMin = models.IntegerField(null=True,blank=True)
-    reserved = models.IntegerField(null=True,blank=True)
-    merged = models.IntegerField(null=True,blank=True)
-    disval = models.FloatField(null=True,blank=True)
+    occupied = models.IntegerField(null=True, blank=True)
+    occHrs = models.IntegerField(null=True, blank=True)
+    occMin = models.IntegerField(null=True, blank=True)
+    reserved = models.IntegerField(null=True, blank=True)
+    merged = models.IntegerField(null=True, blank=True)
+    disval = models.FloatField(null=True, blank=True)
 
     def __str__(self) :
         return self.title
-
 
 
 class Order(models.Model) :
     order_date = models.DateField(auto_now=True)
     table = models.ForeignKey(Table, on_delete=models.CASCADE)
     menu = models.ForeignKey(Menu, on_delete=models.CASCADE)
-    quantity = models.IntegerField(null=False,blank=False)
-    printsts = models.IntegerField(null=True,blank=True)
-    orderedby = models.ForeignKey(User,on_delete=models.CASCADE,null=True,blank=True)
+    quantity = models.IntegerField(null=False, blank=False)
+    printsts = models.IntegerField(null=True, blank=True)
+    orderedby = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
 
     def __str__(self) :
         table = self.table.title
@@ -101,31 +102,33 @@ class Order(models.Model) :
         return table + dash + menu
 
 
-class Bill(models.Model):
-    fiscalyrs = models.CharField(max_length=50,null=True,blank=True)
-    billnum = models.CharField(max_length=50,null=True,blank=True)
-    bill_date = models.CharField(max_length=50,null=True)
-    table = models.CharField(max_length=50,null=True,blank=True)
-    amnt = models.FloatField(null=True,blank=True)
-    discount = models.FloatField(null=True,blank=True)
-    taxable_amnt = models.FloatField(null=True,blank=True)
-    tax_amnt = models.FloatField(null=True,blank=True)
-    total_amnt = models.FloatField(null=True,blank=True)
-    sync_ird = models.IntegerField(null=True,blank=True)
-    billprt = models.IntegerField(null=True,blank=True)
-    billactive = models.IntegerField(null=True,blank=True)
-    bill_time = models.TimeField(auto_now=True,null=True)
-    billuser = models.CharField(max_length=64,null=True,blank=True)
-    is_realtime = models.BooleanField(null=True,blank=True)
-    payment_method = models.CharField(max_length=50,null=True,blank=True)
+class Bill(models.Model) :
+    fiscalyrs = models.CharField(max_length=50, null=True, blank=True)
+    billnum = models.CharField(max_length=50, null=True, blank=True)
+    bill_date = models.CharField(max_length=50, null=True)
+    table = models.CharField(max_length=50, null=True, blank=True)
+    amnt = models.FloatField(null=True, blank=True)
+    discount = models.FloatField(null=True, blank=True)
+    taxable_amnt = models.FloatField(null=True, blank=True)
+    tax_amnt = models.FloatField(null=True, blank=True)
+    total_amnt = models.FloatField(null=True, blank=True)
+    sync_ird = models.IntegerField(null=True, blank=True)
+    billprt = models.IntegerField(null=True, blank=True)
+    billactive = models.IntegerField(null=True, blank=True)
+    bill_time = models.TimeField(auto_now=True, null=True)
+    billuser = models.CharField(max_length=64, null=True, blank=True)
+    is_realtime = models.BooleanField(null=True, blank=True)
+    payment_method = models.CharField(max_length=50, null=True, blank=True)
 
     def __str__(self) :
         return self.billnum
 
-class BillNo(models.Model):
+
+class BillNo(models.Model) :
     number = models.IntegerField(unique=True)
 
-class masterPass(models.Model):
+
+class masterPass(models.Model) :
     password = models.CharField(max_length=50)
 
 
@@ -141,16 +144,16 @@ class masterPass(models.Model):
 #     menu = models.ForeignKey(Menu, on_delete=models.CASCADE)
 #     inventory = models.ForeignKey(Inventory, on_delete=models.CASCADE)
 
-class MergeTable(models.Model):
-    table1 = models.ForeignKey(Table,on_delete=models.CASCADE,related_name='table1')
-    table2 = models.ForeignKey(Table,on_delete=models.CASCADE,related_name='table2')
+class MergeTable(models.Model) :
+    table1 = models.ForeignKey(Table, on_delete=models.CASCADE, related_name='table1')
+    table2 = models.ForeignKey(Table, on_delete=models.CASCADE, related_name='table2')
+
 
 # class TableDiscount(models.Model):
 #     table = models.ForeignKey(Table,on_delete=models.CASCADE)
 #     disval = models.FloatField(null=True,blank=True)
 
-class RestoLogs(models.Model):
+class RestoLogs(models.Model) :
     datentime = models.CharField(max_length=50)
     account = models.CharField(max_length=50)
     activity = models.TextField(max_length=250)
-
